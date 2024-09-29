9923322bca368d199b5d4e8bd1b4eae4f98fa670
maliha-afk <24amaliha@gmail.com>
2024-09-29 18:36:09 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+phone_num2=int(input("enter your 1st two digit phone number: "))
+
+phone_num={
+    "indonesia": +62 ,
+    "bangladesh":+88 ,
+    "india":+91 ,
+    "pakistan":+92 ,
+    "australia":+61 ,
+    "South Arabia":+96 ,
+    "denmark": +45,}
+print(phone_num2)
+if phone_num2== phone_num:
+    if phone_num2=="indonesia":
+        print("you are from indonesia")
+    
+    if phone_num2=="bangladesh":
+        print("you are from bangladesh")
+
+    if phone_num2=="india":
+        print("you are from india")
+    
+    if phone_num2=="pakistan":
+        print("you are from pakistan")
+    
+    if phone_num2=="australia":
+        print("you are from australia")
+    
+    if phone_num2=="South Arabia":
+        print("you are from South Arabia")
+    
+    if phone_num2=="denmark":
+        print("you are from denmark")
